999cfa73a5c5834323acf3a7433e9cc86ebed9f2
Bogdan <thegarrybit@gmail.com>
2023-01-03 10:23:50 +0200

    form animations added

--- script.js
+++ script.js
@@ -97,6 +97,8 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_sliders__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/sliders */ "./src/js/modules/sliders.js");
 /* harmony import */ var _modules_modals__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/modals */ "./src/js/modules/modals.js");
+/* harmony import */ var _modules_forms__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/forms */ "./src/js/modules/forms.js");
+
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -105,7 +107,87 @@ window.addEventListener('DOMContentLoaded', () => {
   Object(_modules_sliders__WEBPACK_IMPORTED_MODULE_0__["default"])('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn', '.main-prev-btn');
   Object(_modules_sliders__WEBPACK_IMPORTED_MODULE_0__["default"])('.main-slider-item', 'vertical');
   Object(_modules_modals__WEBPACK_IMPORTED_MODULE_1__["default"])();
+  Object(_modules_forms__WEBPACK_IMPORTED_MODULE_2__["default"])();
 });
+
+/***/ }),
+
+/***/ "./src/js/modules/forms.js":
+/*!*********************************!*\
+  !*** ./src/js/modules/forms.js ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _services_createOrDeleteStatusMessage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../services/createOrDeleteStatusMessage */ "./src/js/services/createOrDeleteStatusMessage.js");
+/* harmony import */ var _services_checkCyrillicInput__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../services/checkCyrillicInput */ "./src/js/services/checkCyrillicInput.js");
+/* harmony import */ var _services_cervices__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../services/cervices */ "./src/js/services/cervices.js");
+
+
+
+const forms = () => {
+  const allForms = document.querySelectorAll('form');
+
+  // checkNumInputs('input[name="user_phone"]')
+  Object(_services_checkCyrillicInput__WEBPACK_IMPORTED_MODULE_1__["default"])('textarea[name="message"]', /[a-z]/gi);
+  Object(_services_checkCyrillicInput__WEBPACK_IMPORTED_MODULE_1__["default"])('input[name="name"]', /[a-z]/gi);
+  Object(_services_checkCyrillicInput__WEBPACK_IMPORTED_MODULE_1__["default"])('input[name="phone"]', /\D/g);
+  const messages = {
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...',
+    loading: 'Загрузка...',
+    ok: 'assets/img/ok.gif',
+    notOk: 'assets/img/fail.png',
+    spinner: 'assets/img/spinner.gif'
+  };
+  function bindPostData(form) {
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const load = document.createElement('img');
+      load.style.scale = '0.2';
+      load.src = messages.spinner;
+      form.parentNode.append(load);
+
+      // const statusMessage = createOrDeleteStatusMessage({
+      //     statusImage: messages.spinner,
+      //     appendElement: form,
+      //     delay: 3000,
+      //     scale: 0.5
+      // });
+      const formData = new FormData(form);
+      Object(_services_cervices__WEBPACK_IMPORTED_MODULE_2__["postData"])('assets/server.php', formData).then(data => {
+        console.log(data);
+        Object(_services_createOrDeleteStatusMessage__WEBPACK_IMPORTED_MODULE_0__["default"])({
+          message: messages.success,
+          statusImage: messages.ok,
+          appendElement: form,
+          oldElement: load,
+          animate: true,
+          delay: 1700,
+          scale: 1
+        });
+      }).catch(() => {
+        Object(_services_createOrDeleteStatusMessage__WEBPACK_IMPORTED_MODULE_0__["default"])({
+          statusImage: messages.notOk,
+          message: messages.failure,
+          appendElement: form,
+          oldElement: load,
+          animate: true,
+          delay: 2000,
+          scale: 1
+        });
+      }).finally(() => {
+        allForms.forEach(form => form.reset());
+      });
+    });
+  }
+  allForms.forEach(form => {
+    bindPostData(form);
+  });
+};
+/* harmony default export */ __webpack_exports__["default"] = (forms);
 
 /***/ }),
 
@@ -263,6 +345,131 @@ const sliders = (slidesSelector, direction, prev, next) => {
   });
 };
 /* harmony default export */ __webpack_exports__["default"] = (sliders);
+
+/***/ }),
+
+/***/ "./src/js/services/cervices.js":
+/*!*************************************!*\
+  !*** ./src/js/services/cervices.js ***!
+  \*************************************/
+/*! exports provided: postData */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "postData", function() { return postData; });
+const postData = async (url, data) => {
+  const res = await fetch(url, {
+    method: 'POST',
+    body: data
+  });
+  const errorMessage = `Could not fetch ${url}, status: ${res.status}`;
+  if (!res.ok) throw new Error(errorMessage);
+  return await res.text();
+};
+
+
+/***/ }),
+
+/***/ "./src/js/services/checkCyrillicInput.js":
+/*!***********************************************!*\
+  !*** ./src/js/services/checkCyrillicInput.js ***!
+  \***********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const checkCyrillicInput = (selector, replace) => {
+  const inputs = document.querySelectorAll(selector);
+  inputs.forEach(e => {
+    e.addEventListener('input', () => {
+      e.value = e.value.replace(replace, '');
+    });
+  });
+};
+/* harmony default export */ __webpack_exports__["default"] = (checkCyrillicInput);
+
+/***/ }),
+
+/***/ "./src/js/services/createOrDeleteStatusMessage.js":
+/*!********************************************************!*\
+  !*** ./src/js/services/createOrDeleteStatusMessage.js ***!
+  \********************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const createOrDeleteStatusMessage = _ref => {
+  let {
+    statusImage,
+    appendElement,
+    delay,
+    scale,
+    message,
+    oldElement,
+    animate
+  } = _ref;
+  try {
+    oldElement.remove();
+    oldElement.statusMessage.remove();
+    oldElement.textMessage.remove();
+  } catch (e) {}
+  appendElement.classList.add('animated', 'fadeOutUp');
+  setTimeout(() => {
+    appendElement.style.display = 'none';
+    statusMessage.classList.add('animated', 'fadeInUp');
+    appendElement.parentNode.append(statusMessage);
+    textMessage.classList.add('animated', 'fadeIn');
+    appendElement.parentNode.append(textMessage);
+  }, 200);
+  const statusMessage = document.createElement('img');
+  const textMessage = document.createElement('div');
+  statusMessage.src = statusImage;
+  statusMessage.style.cssText = `
+        margin: 0 auto
+        display: none;
+        transform: scale(${scale});
+        border-radius: 4px;
+        height: 100%;
+        width: 100%;
+        z-index: 3;
+    `;
+  if (message) {
+    textMessage.innerHTML = message;
+    textMessage.style.cssText = `
+            transform: translateX(50%);
+            background-color: white;
+            border-radius: 4px;
+            font-size: 23px;
+            position: fixed;
+            z-index: 5;
+            right: 50%;
+            top: 98%;
+            width: 100%
+        `;
+  }
+  setTimeout(() => {
+    appendElement.classList.add('fadeIn');
+    appendElement.classList.remove('fadeOutUp');
+    statusMessage.classList.remove('fadeInUp');
+    statusMessage.classList.add('fadeOut');
+    textMessage.classList.add('fadeOut');
+    setTimeout(() => {
+      appendElement.style.display = 'block';
+      statusMessage.remove();
+      textMessage.remove();
+    }, 750);
+  }, delay);
+  statusMessage.classList.remove('fadeOut');
+  appendElement.classList.remove('fadeIn');
+  return {
+    statusMessage,
+    textMessage
+  };
+};
+/* harmony default export */ __webpack_exports__["default"] = (createOrDeleteStatusMessage);
 
 /***/ })
 
