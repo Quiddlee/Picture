c61830f6de65d52f252e5e04925c02397a1738e8
Bogdan <thegarrybit@gmail.com>
2023-01-05 22:13:40 +0200

    collapse animations improvements

--- src/js/modules/collapse.js
+++ src/js/modules/collapse.js
@@ -9,7 +9,18 @@ const collapse = (triggersSelector, descrSelector) => {
 
 
     buttons.forEach(button => {
-        button.addEventListener('click', () => {
+        button.addEventListener('click', function() {
+            if (!this.classList.contains('active')) {
+                // buttons.forEach(button => {
+                //     button.classList.remove('active', 'active-style');
+                // });
+
+                this.classList.add('active', 'active-style');
+            }
+            else {
+                this.classList.remove('active', 'active-style');
+            }
+
             const sibling = button.nextElementSibling;
 
             if (sibling.style.display === 'none') {
@@ -19,7 +30,7 @@ const collapse = (triggersSelector, descrSelector) => {
                     {filter: 'opacity(0%)',
                         transform: 'translateY(-45%) scale(0.5, 0.4)'},
                     {filter: 'opacity(25%)',
-                        transform: 'translateY(-2%)'},
+                        transform: 'translateY(-3%)'},
                     {filter: 'opacity(100%)',
                         transform: 'translateY(0%) scale(1)'}
                 ], {
@@ -27,15 +38,17 @@ const collapse = (triggersSelector, descrSelector) => {
                 });
             }
             else {
-                setTimeout(() => sibling.style.display = 'none', 250);
+                setTimeout(() => sibling.style.display = 'none', 300);
 
                 sibling.animate([
                     {filter: 'opacity(100%)',
                     transform: 'translateY(0) scale(1)'},
                     {filter: 'opacity(25%)',
-                    transform: 'translateY(-45%) scale(0.5, 0.4)'},
+                    transform: 'translateY(30%) scale(0.5, 0.4)'},
+                    {filter: 'opacity(20%)',
+                    transform: 'translateY(-75%) scale(0)'}
                 ], {
-                    duration: 250,
+                    duration: 300,
                 });
             }
         });
