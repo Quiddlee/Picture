17636f87d1b6cc9c53c13ee3231da2f44e37edfa
Bogdan <thegarrybit@gmail.com>
2023-01-06 16:50:52 +0200

    collapse animations improvements

--- src/js/modules/collapse.js
+++ src/js/modules/collapse.js
@@ -3,35 +3,33 @@ const collapse = (triggersSelector, descrSelector) => {
     const blocks = document.querySelectorAll(descrSelector);
 
 
-    blocks.forEach(block => {
-        block.style.display = 'none';
-    });
+    // blocks.forEach(block => {
+    //     block.style.display = 'none';
+    // });
 
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
+            const sibling = this.nextElementSibling;
             this.classList.toggle('active-style');
+            sibling.classList.toggle('active-content');
 
-            const sibling = button.nextElementSibling;
-
-            if (sibling.style.display === 'none') {
-                sibling.style.display = 'block';
+            if (this.classList.contains('active-style')) {
+                sibling.style.maxHeight = `${sibling.scrollHeight + 80}px`;
 
                 sibling.animate([
                     {filter: 'opacity(0%)',
-                        transform: 'translateY(-75%) scale(0.5, 0.4)'},
+                        transform: 'translateY(-120%) scale(0.5, 0.4)'},
                     {filter: 'opacity(25%)',
-                        transform: 'translateY(-3%) scale(1)'},
+                        transform: 'translateY(-3%) scale(1, 1.05)'},
                     {filter: 'opacity(100%)',
-                        transform: 'translateY(0%) '}
+                        transform: 'translateY(0%) scale(1)'}
                 ], {
                     duration: 500,
                 });
             }
             else {
-                setTimeout(() => {
-                    sibling.style.display = 'none'
-                }, 300);
+                sibling.style.maxHeight = '0px';
 
                 sibling.animate([
                     {filter: 'opacity(100%)',
